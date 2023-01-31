87bad69b536867b372b955062538ec539d3b9c4b
Colby Neves <colby.neves@smail.astate.edu>
2023-01-31 17:11:48 -0600

    1

---
+++ src/types/studentTypes.d.ts
@@ -0,0 +1,39 @@
+type CourseGrade = {
+    name: string;
+    weight: number;
+    grade: number;
+};
+
+type CourseGrades = {
+    assignmentWeights: array<CourseGrade>;
+    finalExamWeight: number;
+}
+
+type Student = {
+    name: string;
+    weights: CourseGrades;
+    currentAverage: number;
+}
+
+type NewStudentRequest = {
+    name: string;
+    weights: CourseGrades;
+};
+
+type AssignmentGrade = {
+    grade: number;
+};
+
+type FinalGrade = {
+    overallScore: number;
+    letterGrade: string;
+};
+
+type FinalExamScores = {
+    needForA: number;
+    needForB: number;
+    needForC: number;
+    needForD: number;
+};
+
+type StudentManager = Record<string, Student>;
